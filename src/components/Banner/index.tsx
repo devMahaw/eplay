ae338e7e89b1b28ecc1a4cfbe10627d7bee1c59c
Marcelo Dantas <mahawprogramacao@gmail.com>
2023-12-21 18:01:48 -0300

    feat: componentizing buttons and finalizing the banner section

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,14 +1,26 @@
 import { Image, Title, Prices } from "./styles";
 import bannerImg from "../../assets/images/banner-homem-aranha.png";
+import Tag from "../Tag";
+import Button from "../Button";
 
 const Banner = () => (
   <Image style={{ backgroundImage: `url(${bannerImg})` }}>
     <div className="container">
-      <Title>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Title>
-      <Prices>
-        De <span>R$ 250,00</span> <br />
-        por apenas R$ 99,90
-      </Prices>
+      <Tag size="big">Destaque do dia</Tag>
+      <div>
+        <Title>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Title>
+        <Prices>
+          De <span>R$ 250,00</span> <br />
+          por apenas R$ 99,90
+        </Prices>
+      </div>
+      <Button
+        type="link"
+        to="/produtos"
+        title="Clique aqui para aproveitar esta oferta"
+      >
+        Aproveitar
+      </Button>
     </div>
   </Image>
 );
