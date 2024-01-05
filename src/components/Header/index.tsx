3260c39bd79bc517aea420bc23fed821ca8a8fd9
Marcelo Dantas <mahawprogramacao@gmail.com>
2024-01-05 17:25:05 -0300

    feat: responsive menu included

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { HeaderBar, Links, LinksItem, CartButton, HeaderLink } from "./styles";
+import {
+  HeaderBar,
+  Links,
+  LinksItem,
+  CartButton,
+  HeaderLink,
+  Hamburguer,
+  HeaderRow,
+  NavMobile
+} from "./styles";
 import logo from "../../assets/images/logo.svg";
 import carrinho from "../../assets/images/carrinho.svg";
 import { open } from "../../store/Reducers/cart";
@@ -7,6 +17,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootReducer } from "../../store";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const dispatch = useDispatch();
   const { items } = useSelector((state: RootReducer) => state.cart);
 
@@ -16,30 +27,51 @@ const Header = () => {
 
   return (
     <HeaderBar>
-      <div>
-        <h1 style={{ height: "30px" }}>
-          <Link to="/">
-            <img src={logo} alt="EPLAY" />
-          </Link>
-        </h1>
-        <nav>
-          <Links>
-            <LinksItem>
-              <HeaderLink to="/categories">Categorias</HeaderLink>
-            </LinksItem>
-            <LinksItem>
-              <HeaderLink to="/#on-sale">Promoções</HeaderLink>
-            </LinksItem>
-            <LinksItem>
-              <HeaderLink to="/#coming-soon">Em breve</HeaderLink>
-            </LinksItem>
-          </Links>
-        </nav>
-      </div>
-      <CartButton onClick={openCart}>
-        {items.length} - produto(s)
-        <img src={carrinho} alt="Carrinho" />
-      </CartButton>
+      <HeaderRow>
+        <div>
+          <Hamburguer onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span />
+            <span />
+            <span />
+          </Hamburguer>
+          <h1 style={{ height: "30px" }}>
+            <Link to="/">
+              <img src={logo} alt="EPLAY" />
+            </Link>
+          </h1>
+          <nav>
+            <Links>
+              <LinksItem>
+                <HeaderLink to="/categories">Categorias</HeaderLink>
+              </LinksItem>
+              <LinksItem>
+                <HeaderLink to="/#on-sale">Promoções</HeaderLink>
+              </LinksItem>
+              <LinksItem>
+                <HeaderLink to="/#coming-soon">Em breve</HeaderLink>
+              </LinksItem>
+            </Links>
+          </nav>
+        </div>
+        <CartButton onClick={openCart}>
+          {items.length}
+          <span> - produto(s)</span>
+          <img src={carrinho} alt="Carrinho" />
+        </CartButton>
+      </HeaderRow>
+      <NavMobile className={isMenuOpen ? "is-open" : ""}>
+        <Links>
+          <LinksItem>
+            <HeaderLink to="/categories">Categorias</HeaderLink>
+          </LinksItem>
+          <LinksItem>
+            <HeaderLink to="/#on-sale">Promoções</HeaderLink>
+          </LinksItem>
+          <LinksItem>
+            <HeaderLink to="/#coming-soon">Em breve</HeaderLink>
+          </LinksItem>
+        </Links>
+      </NavMobile>
     </HeaderBar>
   );
 };
